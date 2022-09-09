23f0a6cfa1553e8a119a7ceea55efb87770dcfdb
MewPk <milzaza123@gmail.com>
2022-09-09 20:41:17 +0700

    Update Display Bg and Text Menu !!

--- ChaLamChopNgapKun/ChaLamChopNgapKun/Main.cpp
+++ ChaLamChopNgapKun/ChaLamChopNgapKun/Main.cpp
@@ -1,22 +1,70 @@
 #include <SFML/Graphics.hpp>
+#include <stdlib.h>
+#include "Menu.h"
+
+using namespace menu;
+using namespace sf;
+using namespace std;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    ContextSettings settings;
+    settings.antialiasingLevel = 8;
+    RenderWindow window(VideoMode(1600, 986), "ChaLamChopNgapKun",Style::Default ,settings );
+    render Menu;
+    Menu.draw();
 
+    sf::Texture texture;
+    texture.loadFromFile("src/imgs/bg-under-water.jpg");
+    texture.setSmooth(true);
+    sf::Sprite sprite(texture);
+
+    // Font
+    sf::Text text;
+    sf::Font font;
+
+    font.loadFromFile("src/fonts/KaushanScript-Regular.ttf");
+
+    text.setFont(font);
+    // set the string to display
+    text.setString("ChaLamChopNgapKun");
+
+    // set the character size
+    text.setCharacterSize(24); // in pixels, not points!
+
+    // set the color
+    text.setFillColor(sf::Color::White);
+
+    // set the text style
+    text.setStyle(sf::Text::Bold );
+
+    text.setPosition(700, 493);
+
+
+    if (font.loadFromFile("src/fonts/KaushanScript-Regular.ttf"))
+    {
+
+    }
+
+    if (!texture.loadFromFile("src/imgs/bg-under-water.jpg"))
+    {
+
+    }
     while (window.isOpen())
     {
-        sf::Event event;
+        Event event;
         while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
+        {   
+            if (event.type == Event::Closed)
                 window.close();
+       
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(sprite);
+        window.draw(text);
+      
+        
         window.display();
     }
 
---
+++ ChaLamChopNgapKun/ChaLamChopNgapKun/Menu.h
@@ -0,0 +1,15 @@
+#include <SFML/Graphics.hpp>
+#include <stdlib.h>
+using namespace sf;
+using namespace std;
+
+namespace menu {
+	class render {
+	public :
+		void draw() {
+           
+
+		}
+	
+	};
+}
